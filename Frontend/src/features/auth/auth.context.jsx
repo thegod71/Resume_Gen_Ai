8ed59d562a63d993700630fd0141d6039468bebf
thegod71 <1011gauravsingh@gmail.com>
2026-06-02 19:36:42 +0530

    context of auth Created

---
+++ Frontend/src/features/auth/auth.context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+// AuthProvide why this is needed because hm authcontext.provider ko wrap ess ka under wrap kr sake taki jo bhi component is AuthProvider ke under hoga wo is context ka access kr sake aur use kar sake. Aur isme hm user aur setUser ko state me rakh rahe hai taki jab bhi user login ya logout kare to hm user ki state ko update kr sake aur isse hamare app me user ki information available ho jaye.
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setloading] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, loading, setloading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+//The Context API in React is a way to share data between components without passing props manually through every level of the component tree.
+
+//It helps solve the problem of prop drilling, where data has to be passed through many intermediate components that don't actually use it.
--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,18 +1,55 @@
 import axois from "axios";
+// because  multiple things repeated  in the code like url and withCredentials we can create a instance of axios and set the base url and withCredentials in the instance so that we don't have to repeat it in every request
+const api = axois.create({
+  baseURL: "http://localhost:3000/api/auth/",
+  withCredentials: true,
+});
+
 export async function register({ username, email, password }) {
   try {
-    const response = await axios.post(
-      "http://localhost:3000/api/auth/register",
+    const response = await api.post(
+      "register",
       {
         username,
         email,
         password,
       },
-      {
-        // it is use because in the backend cookies are set and we need to send cookies in the request without this the cookies will not be sent and the user will not be authenticated
-        withCredentials: true,
-      },
+      // ,
+      // { // ya 4 may hoga but instead of writing it in every request we can set it in the instance of axios and then we don't have to write it in every request
+      //   // it is use because in the backend cookies are set and we need to send cookies in the request without this the cookies will not be sent and the user will not be authenticated
+      //   withCredentials: true,
+      // },
     );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+export async function login({ email, password }) {
+  try {
+    const response = await api.post("login", {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function logout() {
+  try {
+    const response = await api.post("logout");
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getMe() {
+  try {
+    const response = await api.get("get-me");
+    return response.data;
   } catch (err) {
     console.log(err);
   }
