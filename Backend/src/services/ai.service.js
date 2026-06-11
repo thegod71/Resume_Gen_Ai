1c86bc29c5707840cbf64a6d83bd6fa4903d04c4
thegod71 <1011gauravsingh@gmail.com>
2026-06-11 18:22:24 +0530

    The Output  for ai report is created

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,18 +1,121 @@
 const { GoogleGenAI } = require("@google/genai");
+const { z } = require("zod");
+
+const { zodToJsonSchema } = require("zod-to-json-schema");
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_GENAI_API_KEY,
 });
 
-async function invokeGeminiAi() {
-  console.log("this is call of ai ");
+const interviewReportSchema = z.object({
+  matchScore: z
+    .number()
+    .describe(
+      "A score between 0 and 100 indicating how well the candidate's profile matches the job describe",
+    ),
+  technicalQuestions: z
+    .array(
+      z.object({
+        question: z
+          .string()
+          .describe("The technical question can be asked in the interview"),
+        intention: z
+          .string()
+          .describe("The intention of interviewer behind asking this question"),
+        answer: z
+          .string()
+          .describe(
+            "How to answer this question, what points to cover, what approach to take etc.",
+          ),
+      }),
+    )
+    .describe(
+      "Technical questions that can be asked in the interview along with their intention and how to answer them",
+    ),
+  behavioralQuestions: z
+    .array(
+      z.object({
+        question: z
+          .string()
+          .describe("The technical question can be asked in the interview"),
+        intention: z
+          .string()
+          .describe("The intention of interviewer behind asking this question"),
+        answer: z
+          .string()
+          .describe(
+            "How to answer this question, what points to cover, what approach to take etc.",
+          ),
+      }),
+    )
+    .describe(
+      "Behavioral questions that can be asked in the interview along with their intention and how to answer them",
+    ),
+  skillGaps: z
+    .array(
+      z.object({
+        skill: z.string().describe("The skill which the candidate is lacking"),
+        severity: z
+          .enum(["low", "medium", "high"])
+          .describe(
+            "The severity of this skill gap, i.e. how important is this skill for the job and how much it can impact the candidate's chances",
+          ),
+      }),
+    )
+    .describe(
+      "List of skill gaps in the candidate's profile along with their severity",
+    ),
+  preparationPlan: z
+    .array(
+      z.object({
+        day: z
+          .number()
+          .describe("The day number in the preparation plan, starting from 1"),
+        focus: z
+          .string()
+          .describe(
+            "The main focus of this day in the preparation plan, e.g. data structures, system design, mock interviews etc.",
+          ),
+        tasks: z
+          .array(z.string())
+          .describe(
+            "List of tasks to be done on this day to follow the preparation plan, e.g. read a specific book or article, solve a set of problems, watch a video etc.",
+          ),
+      }),
+    )
+    .describe(
+      "A day-wise preparation plan for the candidate to follow in order to prepare for the interview effectively",
+    ),
+  title: z
+    .string()
+    .describe(
+      "The title of the job for which the interview report is generated",
+    ),
+  latex: z
+    .string()
+    .describe(
+      "Give me the latex code which i compile on ovverleaf and make a resume or what keywords should change So that my resume is shortlist for the  interview and these change based on the job description",
+    ),
+});
+async function generateInterviewReport({
+  resume,
+  selfDescription,
+  jobDescription,
+}) {
+  const promt = `Generate an Interview report for a candidate with following details
+  Resume:${resume}
+  Sefl Description: ${selfDescription}
+  Job Description: ${jobDescription}`;
 
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
+    contents: "",
+    config: {
+      responseMineType: "application//json", // how ai generate the respone
+      responseJsonSchema: zodToJsonSchema(interviewReportSchema), // formate of the response
+    },
   });
 
   console.log(response.text);
 }
-
 module.exports = invokeGeminiAi;
