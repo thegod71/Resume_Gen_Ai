f6cc858c750ceb13827ae40ae9b278e5025470af
thegod71 <1011gauravsingh@gmail.com>
2026-06-30 19:08:13 +0530

    the reports frontend

--- Frontend/src/features/interview/hooks/useInterview.js
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -4,11 +4,13 @@ import {
   getAllInterviewReports,
 } from "../services/interview.api";
 
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { InterviewContext } from "../interview.context";
+import { useParams } from "react-router";
 
 export const useInterview = () => {
   const context = useContext(InterviewContext);
+  const { interviewId } = useParams();
   if (!context) {
     throw new Error("useInterview must be used within an InterviewProvider");
   }
@@ -21,8 +23,9 @@ export const useInterview = () => {
     resumeFile,
   }) => {
     setLoading(true);
+    let response = null;
     try {
-      const response = await generateInterviewReport({
+      response = await generateInterviewReport({
         jobDescription,
         selfDescription,
         resumeFile,
@@ -33,30 +36,35 @@ export const useInterview = () => {
     } finally {
       setLoading(false);
     }
+    return response.interviewReport;
   };
 
   const getReportById = async (interviewId) => {
     setLoading(true);
+    let response = null;
     try {
-      const response = await getInterviewReportById(interviewId);
+      response = await getInterviewReportById(interviewId);
       setReport(response.interviewReport);
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false);
     }
+    return response.interviewReport;
   };
 
   const getReports = async () => {
     setLoading(true);
+    let response = null;
     try {
-      const response = await getInterviewReportById(interviewId);
+      response = await getInterviewReportById(interviewId);
       setReport(response.interviewReports);
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false);
     }
+    return response.interviewReports;
   };
 
   return {
